84f42878156acdb45737ff62b11f1615df83bfa1
Phong <nghaiphong208@gmail.com>
2026-01-12 10:02:34 +0700

    fixed bg & displaying courses

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,11 +6,11 @@ function Index() {
       <main className="p-20">
         <p className="text-white text-8xl font-semibold text-center mt-4 md:mt-16">Code<span className="text-accent">ded</span></p>
         <div className="w-[75%] m-auto">
-          <p className="text-white mt-4 text-center text-xl text-wrap">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam tempore tempora illo nisi dicta voluptatibus pariatur ut quis incidunt, itaque amet, doloribus placeat praesentium distinctio at id libero laudantium. Quasi.</p>
+          <p className="text-white mt-4 text-center text-2xl text-wrap">Nền tảng học lập trình dành riêng cho học sinh</p>
         </div>
         <ExampleCode />
         <div className="w-[75%] m-auto bg-primary mt-80">
-          <p className="text-white text-8xl font-semibold">Why?</p>
+          <p className="text-white text-6xl font-semibold">Tại sao chọn chúng tôi?</p>
           <div className="flex">
             <p className="text-white text-xl mt-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, atque perspiciatis eos, officia ab est doloribus placeat sunt at culpa, voluptatibus vero vel tempora accusantium quos sed dolor optio numquam.</p>
             <img src="src/img/ohno.png" alt="temp" className="" />
--- src/pages/practice.jsx
+++ src/pages/practice.jsx
@@ -1,15 +1,35 @@
 import Course from "../components/course.jsx";
-import { onValue, ref } from "firebase/database";
+import { ref, get, child } from "firebase/database";
 import { database } from "../../firebase/firebase.js";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { toast, ToastContainer } from "react-toastify";
 
 function Practice() {
+  const [courses, setCourses] = useState();
+
+  useEffect(() => {
+    const dbRef = ref(database);
+    get(child(dbRef, `courses/`)).then((snapshot) => {
+      if (snapshot.exists()) {
+        setCourses(snapshot.val());
+        console.log(snapshot.val());
+      }
+    }).catch((error) => {
+      toast.error(error);
+    });
+  }, []);
+
   return (
     <>
       <main className="flex gap-10 p-20 pt-30">
-        {Object.entries(data).map(([, value]) => (
-          <Course title={value.info.name} content={value.info.desc} />
-        ))}
+        <ToastContainer />
+        {courses ? (
+          Object.entries(courses).map(([, value]) => (
+            <Course key={value.name} title={value.name} content={value.desc} />
+          ))
+        ) : (
+          <p>Loading...</p>
+        )}
       </main>
     </>
   )
